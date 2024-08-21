35ce0597d9925e19e9b4136eac0e031cf7b88a44
ronikapav <ronikapav@gmail.com>
2024-08-21 14:02:12 +0300

    удаление не нужной информации из Recipe.module.scss и RecipeSearch.js

--- src/features/search/ui/RecipeSearch.js
+++ src/features/search/ui/RecipeSearch.js
@@ -32,32 +32,3 @@ const RecipeSearch = () => {
 };
 
 export default RecipeSearch;
-
-//
-/*import React from 'react';
-import { useRecipeSearch } from '../model/useRecipeSearch';
-import Input from '../../../shared/ui/Input/Input';
-import Button from '../../../shared/ui/Button/Button';
-import styles from './RecipeSearch.module.scss';
-
-const RecipeSearch = () => {
-  const { mySearch, handleSearch, handleSubmit } = useRecipeSearch();
-
-  return (
-    <div className={styles.main}>
-      <div className={styles.section_title}>
-        <h1>
-          <span>Discover real flavor treasures within this app</span> Taste Trove
-        </h1>
-        <form className={styles.search} onSubmit={handleSubmit}>
-          <Input placeholder="Enter the ingredient..." type="text" onChange={handleSearch} value={mySearch} />
-          <Button type="submit">
-            Find
-          </Button>
-        </form>
-      </div>
-    </div>
-  );
-};
-
-export default RecipeSearch;*/
